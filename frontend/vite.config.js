116e4d0fc43bc484c544f7ab37619c3f1c1613c0
vay-tk <vaytk001@gmail.com>
2025-09-27 18:02:54 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,17 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    host: true,
+    allowedHosts: [
+      'new-face-frontend-production.up.railway.app'
+    ],
+    cors: true, // optional if making cross-origin requests
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   },
